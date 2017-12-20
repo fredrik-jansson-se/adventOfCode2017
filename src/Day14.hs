b10acb169fb6f5e894c1dc395e64c07372430f30
Fredrik Jansson <frjansso@cisco.com>
2017-12-19 19:40:09 -0800

    Fixed Day12

--- src/Day14.hs
+++ src/Day14.hs
@@ -7,6 +7,8 @@ import Protolude
 import qualified Day10 as D10
 import qualified Data.Text as T
 import TextShow (showt)
+-- import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 
 append :: Text -> Int -> Text
 append txt idx = T.concat [txt, "-", showt idx]
@@ -32,6 +34,33 @@ bits 'd' = 3
 bits 'e' = 3
 bits 'f' = 4
 
+toMap :: Char -> [Int]
+toMap '0' = [0, 0, 0, 0]
+toMap '1' = [0, 0, 0, 1]
+toMap '2' = [0, 0, 1, 0]
+toMap '3' = [0, 0, 1, 1]
+toMap '4' = [0, 1, 0, 0]
+toMap '5' = [0, 1, 0, 1]
+toMap '6' = [0, 1, 1, 0]
+toMap '7' = [0, 1, 1, 1]
+toMap '8' = [1, 0, 0, 0]
+toMap '9' = [1, 0, 0, 1]
+toMap 'a' = [1, 0, 1, 0]
+toMap 'b' = [1, 0, 1, 1]
+toMap 'c' = [1, 1, 0, 0]
+toMap 'd' = [1, 1, 0, 1]
+toMap 'e' = [1, 1, 1, 0]
+toMap 'f' = [1, 1, 1, 1]
+
+toMapRow :: Text -> [Int]
+toMapRow txt = concat $ map toMap $ T.unpack txt
+
+type Pos = (Int, Int)
+type LU = S.Set Pos
+
+rowToLu :: LU -> (Int, [Int]) -> LU
+rowToLu lu (row, bits) = foldl' (\m (v,col) -> if v == 1 then S.insert (row, col) m else m) lu (zip bits [0..])
+
 cntBits :: Text -> Int
 cntBits txt = cb 0 $ T.unpack txt
   where
@@ -45,3 +74,35 @@ solve1 key = let
   cnts = map cntBits hashs
   in 
     sum cnts
+
+isNeighbor :: Pos -> Pos -> Bool
+isNeighbor (r1, c1) (r2, c2) = 1 == dr + dc
+  where
+    dr = abs $ r2 - r1
+    dc = abs $ c2 - c1
+
+removeNeighbors :: LU -> Pos -> LU
+removeNeighbors lu p = let
+  nbrs = S.filter (isNeighbor p) lu
+  lu' = S.delete p lu
+  lu'' = foldl' removeNeighbors lu' nbrs
+  in
+    lu''
+
+countNbrs :: LU -> Int -> Int
+countNbrs lu cnt | S.size lu == 0 = cnt
+countNbrs lu cnt = let
+  lu' = removeNeighbors lu $ S.elemAt 0 lu
+  cnt' = cnt + 1
+  in 
+    countNbrs lu' cnt'
+
+solve2 :: Text -> Int
+solve2 key = let
+  inp = inputs key
+  hashs = map (D10.hash 256) inp
+  mm = map toMapRow hashs :: [[Int]]
+  lu = foldl' rowToLu S.empty $ zip [0..] mm
+  cnt = countNbrs lu 0
+  in 
+    cnt
